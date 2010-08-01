5e7d66b02eb5897e842b1bc43df20db155747205
Martin Husemann <martin@NetBSD.org>
2010-08-01 16:24:00 +0000

    More cut/insert marker fixes

--- help/update-index.pl
+++ help/update-index.pl
@@ -4,7 +4,9 @@ open(IN, "<../www/help/rawrite32-en.htm") || die("could not open input file");
 open(OUT, ">./rawrite32-en.htm") || die("could not open output file");
 while(<IN>) {
 	s/\<\!-- for help: cut start --\>/\<\!-- for help: cut start/o;
+	s/\<\!-- for help: cut end --\>/     for help: cut end --\>/o;
 	s/\<\!-- for help: insert start/\<\!-- for help: insert start --\>/o;
+	s/     for help: insert end --\>/\<\!-- for help: insert end --\>/o;
 	print OUT "$_";
 }
 close(IN);
