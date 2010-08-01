3ac766dcffa7c1dd06e95784398b9875e58fabaa
Martin Husemann <martin@NetBSD.org>
2010-08-01 15:54:29 +0000

    Adapt to new comment style on web page

--- help/update-index.pl
+++ help/update-index.pl
@@ -3,8 +3,8 @@
 open(IN, "<../www/help/rawrite32-en.htm") || die("could not open input file");
 open(OUT, ">./rawrite32-en.htm") || die("could not open output file");
 while(<IN>) {
-	s/\<\!--- for help: cut start ---\>/\<\!--- for help: cut start/o;
-	s/\<\!--- for help: insert start/\<\!--- for help: insert start ---\>/o;
+	s/\<\!-- for help: cut start --\>/\<\!-- for help: cut start/o;
+	s/\<\!-- for help: insert start/\<\!-- for help: insert start --\>/o;
 	print OUT "$_";
 }
 close(IN);
